5b5b8a3480df0975765cb1a11900529f4775dfd7
坂本龍介 <rspepeg@gmail.com>
2023-06-19 00:42:24 +0900

    github actionsのため一階層深いディレクトリを参照するよう修正

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Autonomous_AI_Dialogues/'
+    : '/',
   chainWebpack: (config) => {
     config.module.rules.delete("eslint");
   },
